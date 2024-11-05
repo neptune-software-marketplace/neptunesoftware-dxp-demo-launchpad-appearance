1c501f41793ce62ed717602faf03376f94143b35
Morten Prom <morten@prom.no>
2024-11-05 12:24:22 +0000

    Open each player in a dedicated sidepanel tab

--- artifacts/Application/dxp_demo_nhl_standings/event/sap.m.ColumnListItem/itemTabStandings/press.js
+++ artifacts/Application/dxp_demo_nhl_standings/event/sap.m.ColumnListItem/itemTabStandings/press.js
@@ -10,46 +10,15 @@ apiNHLRosterTeamSeason({
     modelTabForwards.setData(obj.forwards);
     modelTabDefensemen.setData(obj.defensemen);
     modelTabGoalies.setData(obj.goalies);
-    // const roster = [...obj.forwards, ...obj.defensemen, ...obj.goalies];
-    // modelTabRoster.setData(roster);
+});
 
-    // TabRoster.getBinding("items").sort(
-    //     new sap.ui.model.Sorter("positionCode", false,
 
-    //         //group function
-    //         function(context) {
-
-    //             var data = context.getModel().getObject(context.getPath());
-    //             if (data.positionCode === "G") {
-    //                 return {
-    //                     key: "goalie",
-    //                     text: "GOALIES"
-    //                 };
-    //             }
-    //             if (data.positionCode === "D") {
-    //                 return {
-    //                     key: "defensemen",
-    //                     text: "DEFENSEMEN"
-    //                 };
-    //             }
-    //             return {
-    //                 key: "forwards",
-    //                 text: "FORWARDS"
-    //             };
-    //         },
-
-    //         // sorter function
-    //         function(value1, value2) {
-
-    //             var convertedValue1 = value1; // myConvertToNumberFn(value1);
-    //             var convertedValue2 = value2; // myConvertToNumberFn(value2);
-
-    //             if (convertedValue1 < convertedValue2) return -1;
-    //             if (convertedValue1 === convertedValue2) return 0;
-    //             if (convertedValue1 > convertedValue2) return 1;
-    //         }
-    //     )
-    // );
+apiNHLScheduleTeamSeason({
+    team: data.teamAbbrev.default,
+    season: `20242025`
+}).success((obj) => {
+    console.table(obj.games);
+    modelListSchedule.setData(obj.games);
 });
 
 iconTabBarDetail.setVisible();
